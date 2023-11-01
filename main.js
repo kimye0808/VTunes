bdd0e2c3ab230f02d981d15d85d81618d09f8026
timel2ss <dyddn0823@hanyang.ac.kr>
2023-11-01 17:09:03 +0900

    chore: electron 환경 설정

---
+++ main.js
@@ -0,0 +1,31 @@
+const {app, BrowserWindow, protocol} = require('electron')
+const isDev = require("electron-is-dev");
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: false,
+    }
+  });
+  win.loadURL("http://localhost:3000");
+  // win.loadFile('./public/index.html');
+  // win.webContents.openDevTools();
+}
+app.whenReady().then(() => {
+  protocol.registerFileProtocol('local', (request, callback) => {
+    const url = request.url.substr
+    const pathname = decodeURIComponent(request.url.replace('local://', ''));
+    try {
+      callback(pathname);
+    } catch (error) {
+      console.log(error);
+    }
+  });
+  createWindow();
+})
+app.on('window-all-closed', function () {
+  if (process.platform !== 'darwin') app.quit()
+})
