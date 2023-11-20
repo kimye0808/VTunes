a62335b6da75fc618c6b014550afebf335cefe1a
timel2ss <dyddn0823@hanyang.ac.kr>
2023-11-20 23:32:30 +0900

    feat: 가사 파일 로드 기능 구현 및 음원 로드 기능 수정

--- src/file_manager/MusicFileManager.js
+++ src/file_manager/MusicFileManager.js
@@ -2,18 +2,21 @@ import Music from "../domain/Music";
 
 const musicParser = require('music-metadata')
 const fs = require('fs');
+const path = require('node:path');
+
 export default class MusicFileManager {
-  loadMusicFile(path, thumbnailPath) {
+  loadMusicFile(filePath, thumbnailPath) {
     return new Promise((resolve, reject) => {
-      const stream = fs.createReadStream(path);
+      const stream = fs.createReadStream(filePath);
       musicParser.parseStream(stream, {duration: true})
         .then(metadata => {
-          const { lyrics, artist, album, picture } = metadata.common;
-          const { duration } = metadata.format;
-          let { title } = metadata.common;
+          const {lyrics, artist, album, picture} = metadata.common;
+          const {duration} = metadata.format;
+          let {title} = metadata.common;
 
           if (!title) {
-            title = path.baseName(path); // 테스트 안함
+            const basename = path.basename(filePath);
+            title = basename.slice(0, basename.lastIndexOf('.'));
           }
 
           if (picture && picture.length > 0) {
@@ -21,7 +24,7 @@ export default class MusicFileManager {
             fs.writeFileSync(`${thumbnailPath}/${title}.jpg`, image.data);
           }
 
-          const music = new Music(title, lyrics, parseInt(duration), artist, album, path);
+          const music = new Music(title, lyrics, parseInt(duration), artist, album, filePath);
           resolve(music);
         })
         .catch(error => {
@@ -31,7 +34,32 @@ export default class MusicFileManager {
     })
   }
 
-  loadLyricsFile() {
+  loadLyricsFile(path) {
+    try {
+      const lrcContent = fs.readFileSync(path, 'utf-8');
+      const parsedLyrics = this.#parseLRC(lrcContent);
+      return parsedLyrics;
+    } catch (error) {
+      console.error('Error reading or parsing the LRC file:', error);
+      return null;
+    }
+  }
 
+  #parseLRC(lrcContent) {
+    const lines = lrcContent.split('\n');
+    const lyricObj = {};
+
+    for (const line of lines) {
+      const match = line.match(/\[(\d{2}):(\d{2})\.(\d{2})\](.+)/);
+      if (match) {
+        const minutes = parseInt(match[1], 10);
+        const seconds = parseInt(match[2], 10);
+        const milliseconds = parseInt(match[3], 10) * 10;
+        const time = minutes * 60 + seconds + milliseconds / 1000;
+        const text = match[4];
+        lyricObj[time] = text;
+      }
+    }
+    return lyricObj;
   }
 }
